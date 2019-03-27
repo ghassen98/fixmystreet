53fa419b18072e4aa31034aa5b2ab5260d00aaef
Dave Arter <davea@mysociety.org>
2019-03-27 15:36:23 +0000

    [Hounslow] Display column number on street light asset markers

--- web/cobrands/hounslow/assets.js
+++ web/cobrands/hounslow/assets.js
@@ -65,7 +65,79 @@ fixmystreet.assets.add($.extend(true, {}, defaults, {
     asset_item: 'sign'
 }));
 
-fixmystreet.assets.add($.extend(true, {}, defaults, {
+var pin_prefix = fixmystreet.pin_prefix || document.getElementById('js-map-data').getAttribute('data-pin_prefix');
+
+var streetlight_default = {
+    fillColor: "#FFFF00",
+    fillOpacity: 0.6,
+    strokeColor: "#000000",
+    strokeOpacity: 0.8,
+    strokeWidth: 2,
+    pointRadius: 6
+};
+
+var streetlight_select = {
+    externalGraphic: pin_prefix + "pin-spot.png",
+    fillColor: "#55BB00",
+    graphicWidth: 48,
+    graphicHeight: 64,
+    graphicXOffset: -24,
+    graphicYOffset: -56,
+    backgroundGraphic: pin_prefix + "pin-shadow.png",
+    backgroundWidth: 60,
+    backgroundHeight: 30,
+    backgroundXOffset: -7,
+    backgroundYOffset: -22,
+    popupYOffset: -40,
+    graphicOpacity: 1.0,
+
+    label: "${FeatureId}",
+    labelOutlineColor: "white",
+    labelOutlineWidth: 3,
+    labelYOffset: 65,
+    fontSize: '15px',
+    fontWeight: 'bold'
+};
+
+// The label for street light markers should be everything after the final
+// '/' in the feature's FeatureId attribute.
+// This seems to be the easiest way to perform custom processing
+// on style attributes in OpenLayers...
+var select_style = new OpenLayers.Style(streetlight_select);
+select_style.createLiterals = function() {
+    var literals = Object.getPrototypeOf(this).createLiterals.apply(this, arguments);
+    if (literals.label && literals.label.split) {
+        literals.label = literals.label.split("/").slice(-1)[0];
+    }
+    return literals;
+};
+
+var streetlight_stylemap = new OpenLayers.StyleMap({
+  'default': new OpenLayers.Style(streetlight_default),
+  'select': select_style
+});
+
+var labeled_defaults = $.extend(true, {}, defaults, {
+    select_action: true,
+    stylemap: streetlight_stylemap,
+    feature_code: 'FeatureId',
+    actions: {
+        asset_found: function(asset) {
+          var id = asset.attributes[this.fixmystreet.feature_code] || '';
+          if (id !== '' && id.split) {
+              var code = id.split("/").slice(-1)[0];
+              $('.category_meta_message').html('You have selected column <b>' + code + '</b>');
+          } else {
+              $('.category_meta_message').html('You can pick a <b class="asset-spot">' + this.fixmystreet.asset_item + '</b> from the map &raquo;');
+          }
+        },
+        asset_not_found: function() {
+           $('.category_meta_message').html('You can pick a <b class="asset-spot">' + this.fixmystreet.asset_item + '</b> from the map &raquo;');
+        }
+    }
+});
+
+fixmystreet.assets.add($.extend(true, {}, labeled_defaults, {
     http_options: {
         params: {
             TYPENAME: "Lighting"
