8f78aee17f8842991d2447bea65cc0943f2075df
Struan Donald <struan@exo.org.uk>
2019-05-09 14:08:20 +0100

    fixup! [Buckinghamshire] use buckinghamshire map tiles

--- perllib/FixMyStreet/Map/Buckinghamshire.pm
+++ perllib/FixMyStreet/Map/Buckinghamshire.pm
@@ -2,39 +2,15 @@
 # More JavaScript, for street assets
 
 package FixMyStreet::Map::Buckinghamshire;
-use base 'FixMyStreet::Map::WMTSBase';
+use base 'FixMyStreet::Map::UKCouncilWMTS';
 
 use strict;
 
-sub zoom_parameters {
-    my $self = shift;
-    my $params = {
-        zoom_levels    => scalar $self->scales,
-        default_zoom   => 8,
-        min_zoom_level => 0,
-        id_offset      => 0,
-    };
-    return $params;
-}
+sub default_zoom { 8; }
 
-sub tile_parameters {
-    my $self = shift;
-    my $params = {
-        urls            => [ 'https://maps.buckscc.gov.uk/arcgis/rest/services/Basemapping2018/MapServer/WMTS/tile' ],
-        layer_names     => [ 'Basemapping2018' ],
-        wmts_version    => '1.0.0',
-        layer_style     => 'default',
-        matrix_set      => 'default028mm',
-        suffix          => '.png', # appended to tile URLs
-        size            => 256, # pixels
-        dpi             => 96,
-        inches_per_unit => 39.3701, # BNG uses metres
-        projection      => 'EPSG:27700',
-        origin_x        => -5220400.0,
-        origin_y        => 4470200.0,
-    };
-    return $params;
-}
+sub urls { [ 'https://maps.buckscc.gov.uk/arcgis/rest/services/Basemapping2018/MapServer/WMTS/tile' ] }
+
+sub layer_names{ [ 'Basemapping2018' ] }
 
 sub scales {
     my $self = shift;
@@ -62,7 +38,7 @@ sub copyright {
 sub map_template { 'buckinghamshire' }
 
 sub map_javascript { [
-    '/vendor/OpenLayers/OpenLayers.bristol.js',
+    '/vendor/OpenLayers/OpenLayers.wmts.js',
     '/vendor/OpenLayers.Projection.OrdnanceSurvey.js',
     '/js/map-OpenLayers.js',
     '/js/map-wmts-base.js',
@@ -72,19 +48,5 @@ sub map_javascript { [
     '/cobrands/buckinghamshire/js.js',
     '/cobrands/buckinghamshire/assets.js',
 ] }
-
-# Reproject a WGS84 lat/lon into BNG easting/northing
-sub reproject_from_latlon($$$) {
-    my ($self, $lat, $lon) = @_;
-    my ($x, $y) = Utils::convert_latlon_to_en($lat, $lon);
-    return ($x, $y);
-}
-
-# Reproject a BNG easting/northing into WGS84 lat/lon
-sub reproject_to_latlon($$$) {
-    my ($self, $x, $y) = @_;
-    my ($lat, $lon) = Utils::convert_en_to_latlon($x, $y);
-    return ($lat, $lon);
-}
 
 1;
